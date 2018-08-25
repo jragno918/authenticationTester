34f5bec618b86fc520c6fa4aea9b238de8bc0a3d
Joe Ragno <joseph.ragno3@gmail.com>
2018-08-25 12:22:17 -0400

    initial commit

---
+++ routes/apiRoutes.js
@@ -0,0 +1,16 @@
+var db = require("../models");
+
+module.exports = function(app) {
+  app.post('/api/signup', function(req, res) {
+    db.User.create({
+      email: req.body.email,
+      password: req.body.password
+    }).then(function(res){
+      console.log(res);
+      res.send.json(result);
+    }).catch(function(err) {
+      console.log(err);
+      res.json(err);
+    });
+  })
+}
